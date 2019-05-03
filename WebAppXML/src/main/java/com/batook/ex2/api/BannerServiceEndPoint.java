da7369aca3ec0f03c468191aafb50e665c550cbb
batook <batook@mail.ru>
2019-05-03 20:16:27 +0300

    wsdl

--- WebAppXML/src/main/java/com/batook/ex2/api/BannerServiceEndPoint.java
+++ WebAppXML/src/main/java/com/batook/ex2/api/BannerServiceEndPoint.java
@@ -2,6 +2,8 @@ package com.batook.ex2.api;
 
 import com.batook.ex2.schemas.GetBannerRequest;
 import com.batook.ex2.schemas.GetBannerResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -13,15 +15,19 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 @Endpoint
 public class BannerServiceEndPoint {
     private static final String NAMESPACE_URI = "http://batook.com/ex2/schemas";
+    private static final Logger LOGGER = LoggerFactory.getLogger(BannerServiceEndPoint.class);
 
     @Autowired
     private BannerService service;
 
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getBannerRequest")
+    @PayloadRoot(namespace = NAMESPACE_URI,
+                 localPart = "getBannerRequest")
     @ResponsePayload
     public GetBannerResponse getBanner(@RequestPayload GetBannerRequest request) {
+        LOGGER.info("request id: {}", request.getId());
         GetBannerResponse response = new GetBannerResponse();
-        response.setBanner(service.getBanners().get(0));
+        response.setBanner(service.getBanners()
+                                  .get(0));
 
         return response;
     }
